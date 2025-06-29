543be6a996b585ab9a1c980e4c38cf6eef4baf27
Myles Biss <yoloswagman95@gmail.com>
2025-06-29 09:34:15 -0400

    Adds a basic portfolio for me to test against

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [CommonModule]
 })
 export class App {
-  protected title = 'mapple-sapple';
+  name = 'Myles Biss';
+  bio = 'QA engineer & Game Developer. Into CI/CD, Automation, and Storytelling.';
+  links = [
+    { label: 'GitHub', url: 'https://github.com/Mybiss55' },
+    { label: 'LinkedIn', url: 'https://www.linkedin.com/in/myles-biss-28650024b/' },
+    { label: 'Email', url: 'mailto:mylesbiss@outlook.com' }
+  ];
+skills = [
+  'QA Engineering',
+  'Jenkins',
+  'Mocha (JavaScript Framework)',
+  'Agile Methodologies',
+  'Selenium',
+  'Selenium Grid',
+  'CI/CD',
+  'Style Sheets',
+  'Testing',
+  'Quality Assurance',
+  'QA Automation',
+  'Test Automation',
+  'Selenium Testing',
+  'Gerrit',
+  'Git',
+  'Chai',
+  'JavaScript',
+  'HTML',
+  'Cascading Style Sheets (CSS)',
+  'Docker Compose',
+  'Docker',
+  'Godot Engine',
+  'Unity',
+  'Unreal Engine',
+  'Game Development',
+  'Game Design',
+];
 }
